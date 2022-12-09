92f22054bdbac80f9e2def3b871748185db37f28
ole-kristian-rudjord <s354564@oslomet.no>
2022-12-09 17:05:45 +0100

    Removed unnecessary tags

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,33 +2,31 @@ import React from 'react';
 
 export default function FrostedGlassBox(props) {
   return (
-    <>
-      <div
-        style={{
-          width: `${props.type === 'list' ? '50px' : '200px'}`,
-          height: `${props.type === 'list' ? '50px' : '200px'}`,
-          backgroundColor: `rgba(${props.red},${props.green},${props.blue},${
-            props.opacity / 100
-          })`,
-          backdropFilter: `blur(${props.blur}px)`,
-          boxSizing: 'unset',
-          outline: `${
-            props.type === 'list' && props.isSelected === true
-              ? '3px solid white'
-              : ''
-          }`,
-          flexShrink: '0',
-        }}
-      >
-        {/* Buttons for duplicating, removing and activating boxes are only present for list boxes */}
-        {props.type === 'list' && (
-          <>
-            <button onClick={() => props.onDuplicateBox(props)}>du</button>
-            <button onClick={() => props.onRemoveBox(props.id)}>x</button>
-            <button onClick={() => props.onSetActiveBox(props.id)}>edit</button>
-          </>
-        )}
-      </div>
-    </>
+    <div
+      style={{
+        width: `${props.type === 'list' ? '50px' : '200px'}`,
+        height: `${props.type === 'list' ? '50px' : '200px'}`,
+        backgroundColor: `rgba(${props.red},${props.green},${props.blue},${
+          props.opacity / 100
+        })`,
+        backdropFilter: `blur(${props.blur}px)`,
+        boxSizing: 'unset',
+        outline: `${
+          props.type === 'list' && props.isSelected === true
+            ? '3px solid white'
+            : ''
+        }`,
+        flexShrink: '0',
+      }}
+    >
+      {/* Buttons for duplicating, removing and activating boxes are only present for list boxes */}
+      {props.type === 'list' && (
+        <>
+          <button onClick={() => props.onDuplicateBox(props)}>du</button>
+          <button onClick={() => props.onRemoveBox(props.id)}>x</button>
+          <button onClick={() => props.onSetActiveBox(props.id)}>edit</button>
+        </>
+      )}
+    </div>
   );
 }
--- src/components/BoxList.jsx
+++ src/components/BoxList.jsx
@@ -1,64 +1,65 @@
 import React from 'react';
+import BackgroundOptionsList from './BackgroundOptionsList';
 import Box from './Box';
 
 export default function BoxList(props) {
   return (
-    <>
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'min-content 1fr min-content',
+        width: '100%',
+      }}
+    >
+      <button
+        style={{
+          gridColumn: '1/2',
+        }}
+        onClick={props.onAddNewBox}
+      >
+        Add New Box
+      </button>
+
       <div
         style={{
-          display: 'grid',
-          gridTemplateColumns: 'min-content 1fr min-content',
-          width: '100%',
+          gridColumn: '2/3',
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '20px',
+          padding: '10px 20px',
+          overflowX: 'auto',
+          overflowY: 'hidden',
         }}
       >
-        <button
-          style={{
-            gridColumn: '1/2',
-          }}
-          onClick={props.onAddNewBox}
-        >
-          Add New Box
-        </button>
-
-        <div
-          style={{
-            gridColumn: '2/3',
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '20px',
-            padding: '10px 20px',
-            overflowX: 'auto',
-            overflowY: 'hidden',
-          }}
-        >
-          {/* List of all boxes */}
-          {props.boxes.map((box, index) => (
-            <Box
-              key={index}
-              id={box.id}
-              red={box.red}
-              green={box.green}
-              blue={box.blue}
-              opacity={box.opacity}
-              blur={box.blur}
-              isSelected={box.isSelected}
-              type="list"
-              onDuplicateBox={props.onDuplicateBox}
-              onRemoveBox={props.onRemoveBox}
-              onSetActiveBox={props.onSetActiveBox}
-            ></Box>
-          ))}
-        </div>
-
-        <div
-          style={{
-            gridColumn: '3/4',
-            backgroundColor: 'blueviolet',
-          }}
-        >
-          Change background
-        </div>
+        {/* List of all boxes */}
+        {props.boxes.map((box, index) => (
+          <Box
+            key={index}
+            id={box.id}
+            red={box.red}
+            green={box.green}
+            blue={box.blue}
+            opacity={box.opacity}
+            blur={box.blur}
+            isSelected={box.isSelected}
+            type="list"
+            onDuplicateBox={props.onDuplicateBox}
+            onRemoveBox={props.onRemoveBox}
+            onSetActiveBox={props.onSetActiveBox}
+          ></Box>
+        ))}
       </div>
-    </>
+
+      <div
+        style={{
+          gridColumn: '3/4',
+          position: 'relative',
+          backgroundColor: 'blueviolet',
+        }}
+      >
+        Change background
+        <BackgroundOptionsList></BackgroundOptionsList>
+      </div>
+    </div>
   );
 }
