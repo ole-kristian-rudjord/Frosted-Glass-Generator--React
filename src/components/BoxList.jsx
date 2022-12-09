7d65c94391d038299b5c08eb5e425093d7a11ab7
ole-kristian-rudjord <s354564@oslomet.no>
2022-12-09 14:10:01 +0100

    Removed unnecessary code

--- src/components/BoxList.jsx
+++ src/components/BoxList.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
-import FrostedGlassBox from './Box';
+import Box from './Box';
 
-export default function GlassBoxList(props) {
+export default function BoxList(props) {
   return (
     <>
       <div style={{ display: 'flex' }}>
         <button onClick={props.onAddNewBox}>Add New Box</button>
-        {props.boxes.length > 0 && (
-          <>
-            {props.boxes.map((box, index) => (
-              <FrostedGlassBox
-                key={index}
-                id={box.id}
-                red={box.red}
-                green={box.green}
-                blue={box.blue}
-                opacity={box.opacity}
-                blur={box.blur}
-                isSelected={box.isSelected}
-                type="list"
-                onDuplicateBox={props.onDuplicateBox}
-                onRemoveBox={props.onRemoveBox}
-                onSetActiveBox={props.onSetActiveBox}
-              ></FrostedGlassBox>
-            ))}
-          </>
-        )}
+
+        {props.boxes.map((box, index) => (
+          <Box
+            key={index}
+            id={box.id}
+            red={box.red}
+            green={box.green}
+            blue={box.blue}
+            opacity={box.opacity}
+            blur={box.blur}
+            isSelected={box.isSelected}
+            type="list"
+            onDuplicateBox={props.onDuplicateBox}
+            onRemoveBox={props.onRemoveBox}
+            onSetActiveBox={props.onSetActiveBox}
+          ></Box>
+        ))}
       </div>
     </>
   );
