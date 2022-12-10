7fc370c9c038857f2b4aa1b47d506233c71a74a0
ole-kristian-rudjord <s354564@oslomet.no>
2022-12-10 18:13:17 +0100

    More styling with styled-components

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function FrostedGlassBox(props) {
+export default function Box(props) {
   const Box = styled.div`
-    width: ${props.type === 'list' ? '50px' : '200px'};
-    height: ${props.type === 'list' ? '50px' : '200px'};
+    aspect-ratio: 16/10;
     background-color: rgba(
       ${props.red},
       ${props.green},
@@ -12,23 +11,32 @@ export default function FrostedGlassBox(props) {
       ${props.opacity / 100}
     );
     backdrop-filter: blur(${props.blur}px);
-    box-sizing: unset;
-    outline: ${props.type === 'list' && props.isSelected
-      ? '3px solid white'
-      : ''};
+  `;
+
+  const ListBox = styled(Box)`
+    height: 80px;
+    border-radius: 6px;
     flex-shrink: 0;
+    border: ${props.isSelected ? '3px solid white' : ''};
+  `;
+
+  const MainBox = styled(Box)`
+    height: 320px;
+    border-radius: 12px;
   `;
 
   return (
-    <Box>
+    <>
       {/* Buttons for duplicating, removing and activating boxes are only present for list boxes */}
-      {props.type === 'list' && (
-        <>
+      {props.type === 'list' ? (
+        <ListBox>
           <button onClick={() => props.onDuplicateBox(props)}>du</button>
           <button onClick={() => props.onRemoveBox(props.id)}>x</button>
           <button onClick={() => props.onSetActiveBox(props.id)}>edit</button>
-        </>
+        </ListBox>
+      ) : (
+        <MainBox></MainBox>
       )}
-    </Box>
+    </>
   );
 }
--- src/components/BoxList.jsx
+++ src/components/BoxList.jsx
@@ -1,40 +1,71 @@
 import React, { useState } from 'react';
 import BackgroundOptionsList from './BackgroundOptionsList';
 import Box from './Box';
+import styled from 'styled-components';
 
 export default function BoxList(props) {
   const [changeBackgroundListIsActive, setChangeBackgroundListIsActive] =
     useState(false);
 
-  return (
-    <div
-      style={{
-        position: 'relative',
-        display: 'grid',
-        gridTemplateColumns: 'min-content 1fr min-content',
-        width: '100%',
-      }}
-    >
-      <button
-        style={{
-          gridColumn: '1/2',
-        }}
-        onClick={props.onAddNewBox}
-      >
-        Add New Box
-      </button>
+  const spacing = '20px';
 
-      <div
-        style={{
-          gridColumn: '2/3',
-          display: 'flex',
-          flexDirection: 'row',
-          gap: '20px',
-          padding: '10px 20px',
-          overflowX: 'auto',
-          overflowY: 'hidden',
-        }}
-      >
+  const MainDiv = styled.div`
+    position: relative;
+    display: grid;
+    grid-template-columns: min-content 1fr min-content;
+    justify-content: center;
+    width: 100%;
+    padding: ${spacing};
+    padding-bottom: 0;
+  `;
+
+  const TopBox = styled.button`
+    height: 80px;
+    aspect-ratio: 16/10;
+    border-radius: 6px;
+  `;
+
+  const TopButton = styled(TopBox)`
+    background-color: #00ffff;
+    border: 3px solid white;
+  `;
+
+  const FirstTopButton = styled(TopButton)`
+    grid-column: 1/2;
+  `;
+
+  const LastTopButton = styled(TopButton)`
+    grid-column: 3/4;
+  `;
+
+  const GlassBoxesDiv = styled.div`
+    grid-column: 2/3;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: ${spacing};
+    padding: 0 ${spacing};
+    overflow-x: auto;
+    overflow-y: hidden;
+  `;
+
+  /* const GlassBox = styled(TopBox)`
+    flex-shrink: 0;
+    background-color: rgba(
+      ${(props) => props.red},
+      ${(props) => props.green},
+      ${(props) => props.blue},
+      ${(props) => props.opacity / 100}
+    );
+    backdrop-filter: blur(${(props) => props.blur}px);
+    outline: ${props.isSelected ? '3px solid white' : ''};
+  `; */
+
+  return (
+    <MainDiv>
+      <FirstTopButton onClick={props.onAddNewBox}>Add New Box</FirstTopButton>
+
+      <GlassBoxesDiv>
         {/* List of all boxes */}
         {props.boxes.map((box, index) => (
           <Box
@@ -51,14 +82,29 @@ export default function BoxList(props) {
             onRemoveBox={props.onRemoveBox}
             onSetActiveBox={props.onSetActiveBox}
           ></Box>
+          // <GlassBox
+          //   as="div"
+          //   key={index}
+          //   id={box.id}
+          //   red={box.red}
+          //   green={box.green}
+          //   blue={box.blue}
+          //   opacity={box.opacity}
+          //   blur={box.blur}
+          //   isSelected={box.isSelected}
+          //   // type="list"
+          //   onDuplicateBox={props.onDuplicateBox}
+          //   onRemoveBox={props.onRemoveBox}
+          //   onSetActiveBox={props.onSetActiveBox}
+          // >
+          //   <button onClick={() => props.onDuplicateBox(props)}>du</button>
+          //   <button onClick={() => props.onRemoveBox(box.id)}>x</button>
+          //   <button onClick={() => props.onSetActiveBox(box.id)}>edit</button>
+          // </GlassBox>
         ))}
-      </div>
+      </GlassBoxesDiv>
 
-      <button
-        style={{
-          gridColumn: '3/4',
-          backgroundColor: 'blueviolet',
-        }}
+      <LastTopButton
         id="open-background-options"
         onClick={() =>
           setChangeBackgroundListIsActive(
@@ -66,13 +112,13 @@ export default function BoxList(props) {
           )
         }
       >
-        Change background{' '}
-        {changeBackgroundListIsActive === true ? 'true' : 'false'}
-      </button>
+        Change background
+      </LastTopButton>
+
       <BackgroundOptionsList
         isActive={changeBackgroundListIsActive}
         onClose={() => setChangeBackgroundListIsActive(false)}
       ></BackgroundOptionsList>
-    </div>
+    </MainDiv>
   );
 }
