73161d629c7d84c1e2e656dcc909029b5e0eb572
ole-kristian-rudjord <s354564@oslomet.no>
2022-12-10 16:57:14 +0100

    First attempt at using styled-components

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function FrostedGlassBox(props) {
+  const Box = styled.div`
+    width: ${props.type === 'list' ? '50px' : '200px'};
+    height: ${props.type === 'list' ? '50px' : '200px'};
+    background-color: rgba(
+      ${props.red},
+      ${props.green},
+      ${props.blue},
+      ${props.opacity / 100}
+    );
+    backdrop-filter: blur(${props.blur}px);
+    box-sizing: unset;
+    outline: ${props.type === 'list' && props.isSelected
+      ? '3px solid white'
+      : ''};
+    flex-shrink: 0;
+  `;
+
   return (
-    <div
-      style={{
-        width: `${props.type === 'list' ? '50px' : '200px'}`,
-        height: `${props.type === 'list' ? '50px' : '200px'}`,
-        backgroundColor: `rgba(${props.red},${props.green},${props.blue},${
-          props.opacity / 100
-        })`,
-        backdropFilter: `blur(${props.blur}px)`,
-        boxSizing: 'unset',
-        outline: `${
-          props.type === 'list' && props.isSelected === true
-            ? '3px solid white'
-            : ''
-        }`,
-        flexShrink: '0',
-      }}
-    >
+    <Box>
       {/* Buttons for duplicating, removing and activating boxes are only present for list boxes */}
       {props.type === 'list' && (
         <>
@@ -27,6 +29,6 @@ export default function FrostedGlassBox(props) {
           <button onClick={() => props.onSetActiveBox(props.id)}>edit</button>
         </>
       )}
-    </div>
+    </Box>
   );
 }
